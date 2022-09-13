b13139a165ac4c62462d9634aef3e74dbd95b36a
pedro-leoni <pedroleoni1996@gmail.com>
2022-09-13 20:21:33 -0300

    probando cosas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const apiKey = '4ae2636d8dfbdc3044bede63951a019b'
+    let ciudad = ''
+    const searchCity = () => {
+        console.log('ciudad', ciudad)
+    }
+    const handleChange = (e) => {
+        ciudad += e.target.value
+    }
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+    .then( r => r.json())
+    .then( r => console.log(r))
+</script>
+
+<main>
+   <h1>Wheather App</h1>
+   <form on:submit={searchCity}>
+    <input type="text" name="city" id="city">
+    <button type="submit"> buscar </button>
+   </form>
+</main>
