5c3290ca73b7fdd5400d8808dcc4921dd93f44c3
pedro-leoni <pedroleoni1996@gmail.com>
2022-09-23 19:53:25 -0300

    render condicional + declaraciones reactivas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,45 @@
 <script>
 	const apiKey = '4ae2636d8dfbdc3044bede63951a019b';
+	// estas declaraciones funcionan como estados, la que tiene el export indica que la voy a usar en el html como una prop
 	let ciudad = '';
-	const info = [];
-	const searchCity = (e) => {
-		e.preventDefault();
+	export let info = [];
+	let loading = false;
+	let search = null;
+	// declaracion reactiva, el html estara escuchando cada vez que se ejecute y por ende cambie la info 
+	// es como el useEffect 
+	$:if(search){
 		fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
 			.then((r) => r.json())
-			.then((r) => info.push({ r }));
-	};
+			.then((r) => {
+				if(r.cod === 200){
+					let find = null;
+					info.find((c)=>{
+						if(c.r.id === r.id){
+							return find = true
+						} else {
+							return find = false
+						}
+					})
+					if(find){
+						alert('Esta ciudad ya se encuentra en la lista')
+					} else {
+						info.push({ r })
+					}
+				} else {
+					alert('Ciudad no encontrada')
+				}
+			})
+			.then(()=> loading = false)
+	}
+	// handlers 
+	const handleClick = (e) => {
+		e.preventDefault();
+		search = true 
+		loading = true
+		setTimeout(() => {
+			search = false
+		}, 500);
+	}
 	const handleChange = (e) => {
 		ciudad = e.target.value;
 	};
@@ -17,15 +49,20 @@
 	};
 </script>
 
-<main>
+
 	<h1>Wheather App</h1>
-	<form on:submit={searchCity}>
-		<input type="text" name="city" id="city" on:change={handleChange} />
-		<button type="submit"> buscar </button>
-	</form>
+	<input type="text" name="city" id="city" value={ciudad} on:change={handleChange} />
+	<button on:click={handleClick}> search </button>
 	<button on:click={consolePutoLog}> ver info en consola </button>
 	<div>
-        <h2>Ciudades</h2>
-        
+		<!-- Renderizado condicional -->
+        {#if loading}
+		<p>Loading...</p>
+		{:else}
+			{#if info.length}
+				<p>{info.length} Ciudades en la lista</p>
+			{:else} 
+				<p>Debes buscar una ciudad para ver su informacion climatica</p>
+			{/if}
+		{/if}
     </div>
-</main>
